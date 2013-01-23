57e90780879aa8585f1b2274c4dc05b80fc2540d
Chris Egeland <chris@chrisegeland.com>
2013-01-23 00:24:01 -0500

    Completely useless minor spacing fix

--- config/LocalSettings.php
+++ config/LocalSettings.php
@@ -158,11 +158,11 @@ $wgSVGConverter = 'rsvg';
 // Captcha
 require_once( "$IP/extensions/ConfirmEdit/ConfirmEdit.php" );
 // Begin not-quite-captcha-but-should-help-destroy-spam section
-require_once( "$IP/extensions/ConfirmEdit/QuestyCaptcha.php");
+require_once( "$IP/extensions/ConfirmEdit/QuestyCaptcha.php" );
 $wgCaptchaClass = 'QuestyCaptcha';
 $wgCaptchaQuestions[] = array(
-    'question' => 'Fill in the blank: SYN/HAK, The __________ Hackerspace. Hint: It is our city.',
-    'answer' => 'Akron');
+  'question' => 'Fill in the blank: SYN/HAK, The __________ Hackerspace. Hint: It is our city.',
+  'answer' => 'Akron');
 // End not-quite-captcha-but-should-help-destroy-spam section
 
 require_once( "$IP/extensions/ConfirmEdit/ReCaptcha.php"); 
