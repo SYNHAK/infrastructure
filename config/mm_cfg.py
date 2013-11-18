ad98b936568b29f785a4375df5206fcd3933d8c3
Torrie Fischer <tdfischer@hackerbots.net>
2013-11-17 23:08:04 -0500

    Fix mailman urls

--- config/mm_cfg.py
+++ config/mm_cfg.py
@@ -3,11 +3,11 @@ import pwd, grp
 
 MAILMAN_UID = pwd.getpwnam('mailman')[2]
 MAILMAN_GID = grp.getgrnam('mailman')[2]
-DEFAULT_URL_PATTERN = 'http://%s/mailman/'
+DEFAULT_URL_PATTERN = 'https://%s/mailman/'
 DEFAULT_URL_HOST = 'synhak.org'
 DEFAULT_EMAIL_HOST = 'synhak.org'
 add_virtualhost(DEFAULT_URL_HOST, DEFAULT_EMAIL_HOST)
-PUBLIC_ARCHIVE_URL = 'http://synhak.org/pipermail/%(listname)s'
+PUBLIC_ARCHIVE_URL = 'https://synhak.org/pipermail/%(listname)s'
 VIRTUAL_HOST_OVERVIEW = False
 MTA = 'Postfix'
 POSTFIX_STYLE_VIRTUAL_DOMAINS = [ 'synhak.org' ]
